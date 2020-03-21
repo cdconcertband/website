c0e249c255c0bfdd8e6f43049659358c0135725e
Sophie Giraudo <sophiegiraudo@outlook.com>
2020-03-21 09:23:52 +0800

    adds recaptcha site key

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -131,7 +131,7 @@ class Form extends React.Component {
           </label>             
           <div
             className="g-recaptcha"
-            data-sitekey="6LfKN3kUAAAAAGIM1CbXmaRZx3LIh_W2twn1tzkA"
+            data-sitekey="6LeC3uIUAAAAAOp04A81DzVsV3gFpeIEUf0IGbmn"
           />
           {!!subject && <input type="hidden" name="subject" value={subject} />}
           <input type="hidden" name="form-name" value={name} />
